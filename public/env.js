01de8debd068e0691bf55409eb22bce3a96f1ad7
Miaoxuan Han <hanmiaoxuan@gmail.com>
2021-08-10 22:46:09 +1000

    Tried my best,

--- public/env.js
+++ public/env.js
@@ -1,7 +1,66 @@
 const testButtonFunction=()=>{
   alert('Thank you for clicking')
 }
+const uploadCode=(submission)=>{
+  $.ajax({
+    url: '/api/uploadings',
+    contentType: 'application/json',
+    data: JSON.stringify(submission),
+    type: 'POST',
+    success: function(result){
+      alert('Code successfully uploaded')
+    }
+  })
+}
 
+const newUploading=()=>{
+  let title = $('#title_of_project').val();
+  let author = $('#author').val();
+  let language = $('#language').val();
+  let url = $('#repository').val();
+  let submission = {title,author,language,url};
+  console.log(submission);
+  uploadCode(submission);
+}
+
+const requestUploading=()=>{
+  $.get('/api/uploadings',(submissions)=>{
+    if(submissions.length>0){
+      console.log(submissions);
+      listSubmissions(submissions);
+    }
+  })
+}
+// const savedData = {
+//   title:'abc',
+//   author: 'James',
+//   language: 'Java, HTML',
+//   url: '1234'
+// }
+
+// let submissions=[savedData,savedData];
+
+listSubmissions=(submissions)=>{
+  submissions.forEach(submission =>{
+    console.log(submission)
+    let item='<div class="card blue-grey darken-1">'+
+    '<h2 class="center">Successful validation</h2>'+
+    // '<div class="card blue-grey darken-1">'+
+        '<div class="card-content white-text">'+
+            '<span class="card-title">'+submission.title+'</span>'+
+            '<p>Author: '+submission.author+'</p>'+
+            '<p>Language: '+submission.language+'</p>'+
+            '<a href="'+submission.url+'">repository</a>'+
+        '</div>'+
+        '<div class="card-action">'+
+            '<a class="btn waves-effect white grey-text darken-text-2">details</a>'+
+        '</div>'+
+    '</div>'+
+'</div>'+
+    '<div id="listSubmissions"></div>'
+    $('#listSubmissions').append(item)
+  })
+}
 // connect to the socket
 
 let socket = io();
@@ -13,19 +72,15 @@ socket.on('number', (msg) => {
 
 console.log('test')
 $(document).ready(function(){
-  console.log('Ready')
+  console.log('Ready');
+  requestUploading();
+  // listSubmissions(submissions);
+  $('select').formSelect();
   $('.modal').modal();
   $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true
   });
-  //bind the button
-  $('#testButton').click(testButtonFunction)
-
-  //test get call
-  $.get('/test?user_name="Fantastic User"',(result)=>{
-    console.log(result)
-  })
-
+  // newUploading();
 
 })
--- server.js
+++ server.js
@@ -4,14 +4,77 @@ let app = express();
 //var app = require('express')();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
+const bodyparser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const { Collection } = require("mongodb");
+let codeCollection;
 
-
-
-
+const uri = "mongodb+srv://SIT725:hmxbzfq520@sit-725-2021-week4.t0y7c.mongodb.net/SIT725-t2-week4?retryWrites=true&w=majority";
+const client = new MongoClient(uri,{ useNewUrlParser: true});
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyparser.json());
+
+//This function is used to open the connection
+const creatCollections = (collectionName) =>{
+  client.connect((err,db) =>{
+    codeCollection = client.db().collection(collectionName);
+    if(!err){
+      console.log('MongoDB Connected')
+    }
+    else {
+      console.log("DB Error: ", err);
+      process.exit(1);
+    }
+  })
+}
+
+// const savedData = {
+//   title:'abc',
+//   author: 'James',
+//   language: 'Java, HTML',
+//   url: '1234'
+// }
+
+
+const insertCodes = (upload, callback) =>{
+  codeCollection.insertOne(upload, callback);
+}
+
+const getCodes = (callback) =>{
+  codeCollection.find({}).toArray(callback);
+}
+
+app.get('/api/uploadings',(req,res)=>{
+  getCodes((err,result) =>{
+    if(err) {
+      res.json({statusCode: 400, message: err})
+    }
+    else {
+      res.json({statusCode: 200, message:"Success", data: result})
+    }
+  })
+  // console.log('uploadings requestd');
+  // res.send(submissions);
+})
+
+app.post('/api/uploadings',(req,res)=>{
+  console.log('New code uploaded');
+  console.log('body',req.body);
+  let savedData = req.body;
+  insertCodes(savedData, (err,result)=>{
+    if(err) {
+      res.json({statusCode: 400, message: err})
+    }
+    else {
+      res.json({statusCode: 200, message: "Codes Successfully upladed", data: result})
+    }
+  })
+  // submissions.push(savedData);
+  // res.send({result:200})
+})
 
 app.get("/test", function (request, response) {
   var user_name = request.query.user_name;
@@ -34,6 +97,7 @@ io.on('connection', (socket) => {
 
 http.listen(port,()=>{
   console.log("Listening on port ", port);
+  creatCollections("Codes");
 });
 
 //this is only needed for Cloud foundry 
